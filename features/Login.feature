ff149a05551161e9f6317fc0994505aef2613e14
fakktors <fakktorss@gmail.com>
2020-03-13 23:08:01 +0500

    Zachem ya vse delete?!

---
+++ features/Login.feature
@@ -0,0 +1,5 @@
+Feature: Basic authentication
+  Scenario: User with valid credentials can login
+    Given I open a browser
+    When I login with user name "admin" and password "admin"
+    Then I verify Practical SQA page loaded
---
+++ features/step_definitions/Login.rb
@@ -0,0 +1,18 @@
+path = "D:/Ruby_test/drivers/chromedriver.exe"
+
+
+Given(/^I open a browser$/) do
+  @browser = Selenium::WebDriver.for :chrome, driver_path:path
+  url = "http://www.old.practicalsqa.net"
+  @browser.navigate.to url
+end
+
+When(/^I login with user name "([^"]*)" and password "([^"]*)"$/) do |username, password|
+  @browser.find_element(xpath: "//input[@id='user_login']").send_keys username
+  @browser.find_element(xpath: "//input[@id='user_pass']").send_keys password
+  @browser.find_element(xpath: "//input[@id='wp-submit']").click
+end
+
+Then(/^I verify Practical SQA page loaded$/) do
+  @browser.find_element(xpath: "//h1[@id='site-title']")
+end
